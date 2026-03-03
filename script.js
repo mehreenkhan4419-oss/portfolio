8fc07b97ab6e023ce7e4000398fbfc7b33805627
Mehreen Khan <mehreenkhan4419@gmail.com>
2026-03-03 23:50:26 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const links=document.querySelectorAll("nav a");
+
+links.forEach(link=>{
+  link.addEventListener("click",function(e){
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+    .scrollIntoView({behavior:"smooth"});
+  });
+});
